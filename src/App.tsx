243f36d599ac2ac66c4f5900f0f284fcc6a883a4
ing520 <303114993@qq.com>
2023-08-13 23:16:07 +0800

    refactor2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">11556651</div>;
+  return <div className="App">11ww556651</div>;
 }
 
 export default App;
